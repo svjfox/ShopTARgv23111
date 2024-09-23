2961f44b35425f2d2523ae0ac04a38294f4a9111
Ingvar Derešivski <inde999@gmail.com>
2024-09-23 16:44:26 +0300

    SpaceshipIndex

--- ShopTARgv23/ShopTARgv23/Program.cs
+++ ShopTARgv23/ShopTARgv23/Program.cs
@@ -1,3 +1,6 @@
+using ShopTARgv23.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ShopTARgv23
 {
     public class Program
@@ -8,6 +11,9 @@ namespace ShopTARgv23
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ShopTARgv23Context>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
