86008414847dc1204dd40d0ef641bb153eb35020
VitaliiS <svjfox@gmail.com>
2024-10-07 15:00:33 +0300

    07

---
+++ ShopTARgv23/ShopTARgv23/Controllers/RealEstatesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARgv23.Core.Dto;
+
+namespace ShopTARgv23.Controllers
+{
+    public class RealEstatesController : Controller
+    {
+        //teha index vaade
+        //racendus peab naitama index vaadet koos andmetega
+        //create meetotit ei tee valmis
+        //andmete sisestate andmebaasi kasitsi
+        public IActionResult Index()
+        {
+           
+            var realEstates = new List<RealEstateDto>
+    {
+        new RealEstateDto
+        {
+            Id = Guid.NewGuid(),
+            Location = "City Center",
+            Size = 120.5,
+            RoomNumber = 3,
+            BuildingType = "Apartment",
+            CreatedAt = DateTime.Now.AddMonths(-2),
+            ModifiedAt = DateTime.Now
+        },
+        new RealEstateDto
+        {
+            Id = Guid.NewGuid(),
+            Location = "Suburbs",
+            Size = 240,
+            RoomNumber = 5,
+            BuildingType = "House",
+            CreatedAt = DateTime.Now.AddYears(-1),
+            ModifiedAt = DateTime.Now.AddMonths(-1)
+        }
+    };
+
+            return View(realEstates);
+        }
+    }
+}
---
+++ ShopTARgv23/ShopTARgv23/Models/Spaceships/RealEstate.cs
@@ -0,0 +1,17 @@
+﻿namespace ShopTARgv23.Models.Spaceships
+{
+    public class RealEstate
+    {
+        public Guid? Id { get; set; }
+        public string Location { get; set; }
+        public double Size { get; set; }
+
+        public int RoomNumber { get; set; }
+        public string BuildingType { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
+
+
