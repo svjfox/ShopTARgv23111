0ed8bcedbf9903017e29ea812e8db37e15a27dad
VitaliiS <svjfox@gmail.com>
2024-11-11 14:35:56 +0200

    1111

--- ShopTARgv23/ShopTARgv23.ApplicationServices/Services/ChuckNorrisServices.cs
+++ ShopTARgv23/ShopTARgv23.ApplicationServices/Services/ChuckNorrisServices.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Nancy.Json;
+using ShopTARgv23.Core.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,13 +15,15 @@ namespace ShopTARgv23.ApplicationServices.Services
         {
             string url = "https://api.chucknorris.io/jokes/random";
 
-            using (WebClient client = new WebClient())
+           using (WebClient client = new WebClient())
             {
                 string json = client.DownloadString(url);
-
                 ChuckNorrisJokeDto joke = new JavaScriptSerializer().Deserialize<ChuckNorrisJokeDto>(json);
 
-                dto.Joke = joke.Value;
+                dto.icon_url = joke.icon_url;
+                dto.id = joke.id;
+                dto.url = joke.url;
+                dto.value = joke.value;
             }
 
             return dto;
